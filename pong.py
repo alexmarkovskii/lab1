231aafe5238c435729cf5c20dba26f5276922259
alexmarkovskii <markovskiialex@gmail.com>
2024-05-30 11:53:30 +0300

    Ну заработай8

--- pong.py
+++ pong.py
@@ -23,7 +23,6 @@ import pygame
 import sys
 from tkinter.messagebox import showinfo
 from pygame.locals import *
-import time
 import csv
 
 
@@ -495,7 +494,34 @@ class STAT_MENU():
                         self.name_box.draw_border(self.stat_screen, LIME)
 
                     elif self.name_box.use_button.rect.collidepoint(event.pos):
-                        self.write_statistics([self.name_box.text, self.time, self.score])
+                        try:
+                            with open('statistics.csv', 'a', newline='') as csvfile:
+                                csv_writer = csv.writer(csvfile)
+                                csv_writer.writerow([self.name_box.text, self.time, self.score])
+                            
+                            with open('statistics.csv', 'r', newline='') as csvfile:
+                                csv_reader = csv.reader(csvfile)
+                                data = list(csv_reader)
+
+                            # Сортировка данных по третьему элементу
+                            sorted_data = sorted(data, key=lambda x: x[2])
+                            sorted_data = sorted_data[::-1]
+
+                            # Запись отсортированных данных обратно в CSV файл
+                            with open('statistics.csv', 'w', newline='') as csvfile:
+                                csv_writer = csv.writer(csvfile)
+                                csv_writer.writerows(sorted_data)
+                        except:
+                            with open('statistics.csv', 'a', newline='') as csvfile:
+                                csv_writer = csv.writer(csvfile)
+                                csv_writer.writerow([self.name_box.text, self.time, self.score])
+                        text = self.default_font.render('WRITED!', True, WHITE)
+                        self.stat_screen.blit(text, (self.width//2 - text.get_width()//2, 320))
+                        pygame.display.flip()
+                        pygame.time.delay(2000)
+                        menu = MENU()
+                        menu.draw_menu()
+                        break    
                         
                         text = self.default_font.render('WRITED!', True, WHITE)
                         self.stat_screen.blit(text, (self.width//2 - text.get_width()//2, 320))
